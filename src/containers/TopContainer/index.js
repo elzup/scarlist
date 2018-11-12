e7c070d5538b498fb4d6826fee0de00bcd4d5598
elzup <guild0105@gmail.com>
2018-11-12 17:47:44 +0900

    Add swipable tabs

--- src/containers/TopContainer/index.js
+++ src/containers/TopContainer/index.js
@@ -8,13 +8,24 @@ import RoomListContainer from '../RoomListContainer'
 import { loadData } from './logic'
 import { Page } from '../../components'
 // import * as selectors from './selectors'
+import UserForm from '../UserForm'
+import MacAddrDescription from '../../components/MacAddrDescription'
+import { Tab } from '@material-ui/core'
+import { Tabs } from '@material-ui/core'
+import { Paper } from '@material-ui/core'
+import SwipeableViews from 'react-swipeable-views'
 
 type Props = {
   loadData: typeof loadData,
   roomIds: string[],
 }
 
-class TopContainer extends React.Component<Props> {
+type State = {
+  selectedTab: number,
+}
+
+class TopContainer extends React.Component<Props, State> {
+  state = { selectedTab: 0 }
   componentDidMount() {
     this.props.loadData()
   }
@@ -23,7 +34,37 @@ class TopContainer extends React.Component<Props> {
     return (
       <div>
         <Page>
-          <RoomListContainer />
+          <Tabs
+            value={this.state.selectedTab}
+            onChange={(e, selectedTab: number) => {
+              this.setState({ selectedTab })
+            }}
+            indicatorColor="primary"
+            textColor="primary"
+            fullWidth
+          >
+            <Tab label="ホーム" />
+            <Tab label="設定" />
+          </Tabs>
+          <SwipeableViews
+            axis={'x'}
+            index={this.state.selectedTab}
+            onChangeIndex={(selectedTab: number) => {
+              this.setState({ selectedTab })
+            }}
+          >
+            <div>
+              <RoomListContainer />
+            </div>
+            <div>
+              <Paper>
+                <UserForm />
+              </Paper>
+              <Paper>
+                <MacAddrDescription />
+              </Paper>
+            </div>
+          </SwipeableViews>
         </Page>
       </div>
     )
