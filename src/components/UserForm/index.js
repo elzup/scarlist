ebd011c1f492ecb0017b354cdd62a98e3ee77067
elzup <guild0105@gmail.com>
2018-11-16 20:14:17 +0900

    Add Icon edit navigation

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -3,6 +3,7 @@ import * as React from 'react'
 import type { User } from '../../types'
 import { TextField, Button } from '@material-ui/core'
 import { ImgWrapper, Photo } from '../UserTile'
+import { Typography } from '@material-ui/core'
 
 type Props = {
   user: ?User,
@@ -32,37 +33,43 @@ class UserForm extends React.Component<Props> {
       return <div>loading</div>
     }
     return (
-      <form noValidate autoComplete="off">
-        <ImgWrapper style={{ width: '100px' }}>
-          <Photo data-timeout={true} src={props.user.photoURL} alt="" />
-        </ImgWrapper>
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          <TextField
-            style={{ minWidth: '300px', margin: '8px' }}
-            label="名前"
+      <>
+        <form noValidate autoComplete="off">
+          <ImgWrapper style={{ width: '100px' }}>
+            <Photo data-timeout={true} src={props.user.photoURL} alt="" />
+          </ImgWrapper>
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <TextField
+              style={{ minWidth: '300px', margin: '8px' }}
+              label="名前"
+              disabled={props.loading}
+              inputRef={ref => (this.nameRef = ref)}
+              defaultValue={props.user.name || props.user.displayName}
+            />
+            <TextField
+              style={{ minWidth: '300px', margin: '8px' }}
+              label="MACアドレス"
+              disabled={props.loading}
+              helperText="複数登録はカンマ(,)区切り"
+              inputRef={ref => (this.macRef = ref)}
+              defaultValue={(props.user.macAddrs || []).join(',')}
+            />
+          </div>
+          <Button
+            style={{ marginTop: '10px' }}
+            variant="contained"
             disabled={props.loading}
-            inputRef={ref => (this.nameRef = ref)}
-            defaultValue={props.user.name || props.user.displayName}
-          />
-          <TextField
-            style={{ minWidth: '300px', margin: '8px' }}
-            label="MACアドレス"
-            disabled={props.loading}
-            helperText="複数登録はカンマ(,)区切り"
-            inputRef={ref => (this.macRef = ref)}
-            defaultValue={(props.user.macAddrs || []).join(',')}
-          />
-        </div>
-        <Button
-          style={{ marginTop: '10px' }}
-          variant="contained"
-          disabled={props.loading}
-          color="primary"
-          onClick={this.onClickButton}
-        >
-          保存
-        </Button>
-      </form>
+            color="primary"
+            onClick={this.onClickButton}
+          >
+            保存
+          </Button>
+        </form>
+        <Typography>
+          アイコンはGoogleアカウントのものになります。
+          <a href="https://aboutme.google.com/">変更</a>
+        </Typography>
+      </>
     )
   }
 }
